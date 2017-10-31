830c4af1f93d15a9f083b69b9ca4288aefd3d129
4bic <robyne.kiplangat@gmail.com>
2017-10-31 21:54:28 +0300

    nav-links scroll to section

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,10 +15,26 @@ $(document).ready(function(){
     $('.js--scroll-to-plans').click(function(){
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
+
     $('.js--scroll-to-features').click(function(){
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
 
+    /* nav-link scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 
 });
 
